92a8f9c317e4bfac0b3cf7af08f81b70d8420f87
Suraj Barkale <suraj.barkale@aacapella.com>
2020-09-15 15:18:17 +1000

    Fish needs shellinit

--- config/fish.nix
+++ config/fish.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
   enable = true;
+  shellInit = ''
+    set -p fish_function_path ${pkgs.fish-foreign-env}/share/fish-foreign-env/functions
+    fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  '';
   shellAbbrs = {
     l = "ls";
     lw = "lorri watch --once";
