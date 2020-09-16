0fa37757ae9df75920c5fe9052336a421c350397
Suraj Barkale <suraj.barkale@aacapella.com>
2020-09-16 11:58:50 +1000

    Fix fish init clobbring session variables

--- config/fish.nix
+++ config/fish.nix
@@ -3,6 +3,10 @@
   shellInit = ''
     set -p fish_function_path ${pkgs.fish-foreign-env}/share/fish-foreign-env/functions
     fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+    # Reload session vars after nix initialization
+    set -e __HM_SESS_VARS_SOURCED
+    fenv source /Users/suraj/.nix-profile/etc/profile.d/hm-session-vars.sh > /dev/null
+    set -e fish_function_path[1]
   '';
   shellAbbrs = {
     l = "ls";
