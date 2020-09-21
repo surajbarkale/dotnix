016977a9a5c96eed5100298198e0087c4f1f0716
Suraj Barkale <suraj.barkale@aacapella.com>
2020-09-21 10:05:55 +1000

    Set AWS vault keychain to login

--- config/default.nix
+++ config/default.nix
@@ -2,6 +2,7 @@
   nixpkgs.config = { allowUnfree = true; };
   home.sessionVariables = {
     NIX_PATH = "${config.home.homeDirectory}/.nix-defexpr/channels";
+    AWS_VAULT_KEYCHAIN_NAME = "login";
   };
   home.packages = with pkgs;
     [
