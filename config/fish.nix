15cc6340d76b10ba5ba1bc74850c1f874ac071c9
Suraj Barkale <suraj.barkale@aacapella.com>
2021-02-03 14:17:24 +1100

    Use new fish plugins package path

--- config/fish.nix
+++ config/fish.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   enable = true;
   shellInit = ''
-    set -p fish_function_path ${pkgs.fish-foreign-env}/share/fish-foreign-env/functions
+    set -p fish_function_path ${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d
     fenv source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
     # Reload session vars after nix initialization
     set -e __HM_SESS_VARS_SOURCED
